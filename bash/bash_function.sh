b9b8b4df1c51cac0eb0bf6ec06edb0884c325ea2
Ye Gao <ye_gao@mfe.berkeley.edu>
2025-07-23 12:39:53 -0400

    Add two meta-specific tools

--- bash/bash_function.sh
+++ bash/bash_function.sh
@@ -1,4 +1,5 @@
- 
+# general tools
+
 epochconverter() {
     # This function replicates the functionality of
     # https://www.epochconverter.com/
@@ -14,4 +15,32 @@ epochconverter() {
     fi
 }
 
+# Meta specific tools
 
+mkdir -p ~/files
+touch ~/files/targets
+
+function list_targets {
+  # Usage:
+  #   list_targets [--change <commit>]
+  #   List all relevant targets of the current pending changes.
+  #   If a commit is given, it needs to be prefixed with --change flag.
+  sl status -mn "$@" | xargs -I {} buck2 query "owner({})" 2> /dev/null | sort | uniq | tee ~/files/targets
+}
+
+GREEN='\033[1;92m' # bold green
+RESET='\033[0m' # reset
+
+function buildem {
+  while IFS= read -r target; do
+        build_type=$(buck uquery --output-attribute buck.type $target 2> /dev/null | jq -r ".[\"${target}\"].\"buck.type\"")
+        if [[ $build_type =~ .*test$ ]]; then
+            target=$(echo "test $target")
+        else
+            target=$(echo "build $target")
+        fi
+        echo -e "${GREEN}buck $target${RESET}"
+        buck $target
+    echo
+    done < ~/files/targets
+}
